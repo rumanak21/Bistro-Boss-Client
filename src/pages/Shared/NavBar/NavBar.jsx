02914f8105e2ddadc84c6b3442e673d98e6aa7b4
rumanak21 <rumana2k20@gmail.com>
2024-04-25 23:17:33 +0200

    Banner added and Navbar correction

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -5,16 +5,13 @@ const NavBar = () => {
         <li><a>Item 1</a></li>
         <li>
             <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
+            
         </li>
         <li><a>Item 3</a></li>
     </>
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black max-w-screen-xl text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
