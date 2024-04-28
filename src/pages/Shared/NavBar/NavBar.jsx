826c7642a6b6536e0625308cfef028ef67924b7c
rumanak21 <rumana2k20@gmail.com>
2024-04-29 01:36:36 +0200

    used react helmet to adding title

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
     const navOptions = <>
-        <li><a>Home</a></li>
+        <li><Link to={"/"}>Home</Link></li>
         <li>
             <a>Contact Us</a>
         </li>
-        <li><a>Our Menu</a></li>
+        <li><Link to={"/menu"}>Our Menu</Link></li>
         <li><a>Our Shop</a></li>
     </>
     return (
