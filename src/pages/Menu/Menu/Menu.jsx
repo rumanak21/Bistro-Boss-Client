76d08c9dfafccb03403d80d2c23719b177ca555c
rumanak21 <rumana2k20@gmail.com>
2024-04-29 02:46:33 +0200

    used custom hook for data fetching

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import coverImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu"
+
 
 const Menu = () => {
     return (
@@ -10,11 +10,8 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={coverImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={coverImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={coverImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
+            
+            
         </div>
     );
 };
